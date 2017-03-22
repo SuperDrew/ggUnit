ee782688810c2e269979dad007e7db32a6d7c0e1
Andrew Fraser <a.vector@gmail.com>
2017-03-22 14:52:04 +0000

    Unit tests (#1)
    * Ignore all bin and obj folders
    
    * Add Unit test project

---
+++ ggUnit.UnitTests/CollectiveCheck/CheckUnitTests.cs
@@ -0,0 +1,39 @@
+﻿namespace ggUnit.UnitTests.CollectiveCheck
+{
+    using System;
+
+    using GgUnit.CollectiveCheck;
+    using GgUnit.Generator;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class CheckUnitTests
+    {
+        [Test]
+        [TestCase(100, 30, Category = "ShortRunning")]
+        [TestCase(10000, 100, Category = "LongRunning")]
+        [TestCase(100000, 500, Category = "VeryLongRunning")]
+        public void AreEqualShouldReturnTrue(int numberOfCasesToCheck, int maxLengthOfString = 50)
+        {
+            var testResult = new TestResult<ComparisonTestCase<string>>();
+            var regex = $"^.{{1,{maxLengthOfString}}}$";
+            for (var i = 0; i < numberOfCasesToCheck; i++)
+            {
+                var testString = RegexGenerator.GenerateMatch(regex);
+                var testCase = new ComparisonTestCase<string>
+                                   {
+                                       Actual = testString,
+                                       Expected = testString,
+                                       Name = "Check string comparison is equal."
+                                   };
+                testResult.TestCases.Add(testCase);
+                testCase.Checks.Add(Check.AreEqual(testString, testString, testString));
+            }
+
+            var summary = testResult.CreateResultSummary().ToString();
+            Assert.That(testResult.CalculateResult(), Is.EqualTo(State.Passed), summary);
+            Console.WriteLine(summary);
+        }
+    }
+}
