d60528c40fc88d36a6947c75ba95e8327cb8011e
Andrew Fraser <andrew.fraser@abcam.com>
2017-07-06 14:55:05 +0100

    Fix AssemblyInfo details

--- GgUnit/Properties/AssemblyInfo.cs
+++ GgUnit/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("GgUnit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generate Group stochastic testing framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Abcam")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("GgUnit")]
-[assembly: AssemblyCopyright("Copyright © Abcam 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
